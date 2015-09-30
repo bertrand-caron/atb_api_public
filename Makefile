5070a7b9fdd5111d087d1b33947b905b6c8aa584
Bertrand Caron <bertrand.fx.caron@gmail.com>
2015-09-30 10:42:02 +1000

    Add a Makefile and a setup/install script

---
+++ Makefile
@@ -0,0 +1,5 @@
+install-user:
+	python setup.py install --user
+
+install-sudo:
+	python setup.py install
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='atb_api_public',
+      version="0.1",
+      description="An API (Application Programming Interface) to interact with the ATB (http://compbio.biosci.uq.edu.au/atb)",
+      author='Bertrand Caron',
+      author_email='b.caron@uq.edu.au',
+      py_modules=['atb_api']
+     )
