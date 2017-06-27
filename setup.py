7cc609f4863c13ad1d9ff378b3d6ab42707b75b9
Bertrand Caron <bertrand.fx.caron@gmail.com>
2017-06-28 09:59:05 +1000

    Milestone: Bump up version number

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ else:
 
 setup(
     name='atb_api_public',
-    version='0.1',
+    version='0.2',
     description='An API (Application Programming Interface) to interact with the ATB (http://compbio.biosci.uq.edu.au/atb)',
     author='Bertrand Caron',
     author_email='b.caron@uq.edu.au',
     py_modules=['atb_api'],
     package_dir=package_dir,
-    requires=['yaml', 'json'],
+    requires=['yaml', 'json', 'requests'],
 )
